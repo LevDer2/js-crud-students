9e298264a50cf421db0e76a511c2ff39123bca4a
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-12-12 13:50:56 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+// ! Elements!
+const getStudentsButton = document.querySelector("#get-students-btn");
+const table = document.querySelector(".students-table");
+
 // Функція для отримання всіх студентів
 function getStudents() {
-  // твій код
+  return fetch(`http://localhost:3000/students`).then((res) => res.json());
 }
+
+getStudents().then((res) => console.log(res));
+
+getStudentsButton.addEventListener("click", (event) => {
+  return getStudents().then(renderStudents);
+});
+
 // Функція для відображення студентів у таблиці
 function renderStudents(students) {
-  // твій код
+  table.innerHTML = "";
+    //  * make a new table for students
+    const student = students
+      .map(({id, name, skills, isEnrolled, age, course, email}) => {
+        return `<li class="app-item">${country.name.common}</li>`;
+      })
+      .join("");
+    // * Add our items in the end of the list
+    table.insertAdjacentHTML("beforeend", student);
 }
 
 // Функція для додавання нового студента
