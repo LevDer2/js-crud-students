d58dfbc1402bd2e41425f676a4c6b642cec6ea4e
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-12-13 13:33:33 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 // ! Elements!
 const getStudentsButton = document.querySelector("#get-students-btn");
-const table = document.querySelector(".students-table");
+const table = document.querySelector(".students-tbody");
 
 // Функція для отримання всіх студентів
 function getStudents() {
   return fetch(`http://localhost:3000/students`).then((res) => res.json());
 }
 
-getStudents().then((res) => console.log(res));
+// ! For checking our students
+// getStudents().then((res) => console.log(res));
 
+// ! If we click to this button we will get our students
 getStudentsButton.addEventListener("click", (event) => {
   return getStudents().then(renderStudents);
+});
+
+// ! If we click to this button we will delete one of our students
+table.addEventListener("click", (event) => {
+  const deleteButton = event.target.closest("button.delete-students");
+  if (deleteButton) {
+    return fetch(`http://localhost:3000/students`, {
+      method: "DELETE",
+    }).then(() => deleteStudent);
+  }
 });
 
 // Функція для відображення студентів у таблиці
 function renderStudents(students) {
   table.innerHTML = "";
-    //  * make a new table for students
-    const student = students
-      .map(({id, name, skills, isEnrolled, age, course, email}) => {
-        return `<li class="app-item">${country.name.common}</li>`;
-      })
-      .join("");
-    // * Add our items in the end of the list
-    table.insertAdjacentHTML("beforeend", student);
+
+  const rows = students
+    .map(({ id, name, skills, isEnrolled, age, course, email }) => {
+      const skillsList =
+        skills > "1"
+          ? Object.values(skills).join(", ")
+          : "He/she did not have skils";
+
+      return `
+        <tr>
+          <td>${id}</td>
+          <td>${name}</td>
+          <td>${age}</td>
+          <td>${course}</td>
+          <td>${skillsList}</td>
+          <td>${email}</td>
+          <td>${isEnrolled ? "Так" : "Ні"}</td>
+          <td><button class="delete-students">Видалити студента</button></td>
+        </tr>
+      `;
+    })
+    .join("");
+
+  table.insertAdjacentHTML("beforeend", rows);
 }
 
 // Функція для додавання нового студента
@@ -38,5 +66,5 @@ function updateStudent(id) {
 
 // Функція для видалення студента
 function deleteStudent(id) {
-  // твій код
+  console.log("Yes?");
 }
